a636e34625185a102654f77dec163f21e3ecb23c
Philip Berner <philipberner.berlin@gmail.com>
2024-01-19 10:37:23 +0100

    implemented general game list endpoint

---
+++ source/rest/src/main/kotlin/de/hwrberlin/sweii/tablegames/rest/general/GeneralEndpoint.kt
@@ -0,0 +1,19 @@
+package de.hwrberlin.sweii.tablegames.rest.general
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GeneralEndpoint {
+
+    @GetMapping("/games/general/list")
+    fun gameList(): GameListResponse {
+        return GameListResponse(Game.entries.map { game ->
+            GameListResponse.Game(
+                game.name,
+                game.minPlayerCount,
+                game.maxPlayerCount
+            )
+        }.toList())
+    }
+}
