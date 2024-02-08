d1db28e5b76eb5a0025fd83c85e8e40ddad4d7a6
Philip Berner <philipberner.berlin@gmail.com>
2024-02-08 14:41:00 +0100

    added tests for en passant

---
+++ source/core/chess/src/test/kotlin/ChessSpecialMovesTest.kt
@@ -0,0 +1,36 @@
+import de.hwrberlin.sweii.tablegames.chess.Chess
+import de.hwrberlin.sweii.tablegames.chess.ChessPiece
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class ChessSpecialMovesTest {
+
+    @Test
+    fun enPassant() {
+        val chess = Chess()
+
+        assertTrue { chess.move(0, 1, 0, 3, null) }
+        assertTrue { chess.move(0, 6, 0, 5, null) }
+        assertTrue { chess.move(0, 3, 0, 4, null) }
+        assertTrue { chess.move(1, 6, 1, 4, null) }
+        assertTrue { chess.move(0, 4, 1, 5, null) }
+        assertNull(chess.board[4][1])
+    }
+
+    @Test
+    fun falseEnPassant() {
+        val chess = Chess()
+
+        assertTrue { chess.move(3, 1, 3, 3, null) }
+        assertTrue { chess.move(2, 6, 2, 4, null) }
+        assertTrue { chess.move(3, 3, 3, 4, null) }
+        assertTrue { chess.move(0, 6, 0, 4, null) }
+        assertFalse { chess.move(3, 4, 2, 5, null) }
+        assertEquals(ChessPiece.BLACK_PAWN, chess.board[4][0])
+        assertEquals(ChessPiece.BLACK_PAWN, chess.board[4][2])
+        assertEquals(ChessPiece.WHITE_PAWN, chess.board[4][3])
+    }
+}
