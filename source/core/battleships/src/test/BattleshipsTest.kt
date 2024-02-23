3d271eb25355ef6df599b4bb7ea6e01be42b9638
Zusemat <130046077+Zusemat@users.noreply.github.com>
2024-02-23 16:09:00 +0100

    Added test Class for BattleshipsTest.kt

---
+++ source/core/battleships/src/test/BattleshipsTest.kt
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class BattleshipsTest {
+
+    @Test
+    fun getState() {
+    }
+
+    @Test
+    fun getPlayerOneId() {
+    }
+
+    @Test
+    fun getLastTurn() {
+    }
+
+    @Test
+    fun definePlayerOne() {
+    }
+
+    @Test
+    fun winner() {
+    }
+
+    @Test
+    fun placeShip() {
+    }
+
+    @Test
+    fun attack() {
+    }
+
+    @Test
+    fun getPlayerOne() {
+    }
+
+    @Test
+    fun getPlayerTwo() {
+    }
+}
